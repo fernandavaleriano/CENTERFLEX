7e4854fa154c234c1eea42bd66ae3a0dc778e370
Fernanda Valeriano <fernandaramosvaleriano@gmail.com>
2026-06-22 20:55:37 -0300

    Adiciona config.js, CORS no server e prepara para deploy no Render

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // Altere esta URL quando fizer deploy do backend no Render
 // URL do backend no Render (altere apos o deploy)
 // Exemplo: 'https://centerflex-api.onrender.com'
-const API_URL = '';
+const API_URL = 'https://centerflex.onrender.com';
